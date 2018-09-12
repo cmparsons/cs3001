fb531fecb3948a3fa76af8bf536be4abcfac1521
cmparsons <christianp130@gmail.com>
2018-09-11 23:32:48 -0500

    add pdf report and iterative merge

--- hw1/mergesort.py
+++ hw1/mergesort.py
@@ -1,27 +1,50 @@
 """
 2a Merge Sort
 
-NOTE: Recursive implementation which is not ideal for large datasets due to limited
-stack space.
+NOTE: Recursive implementation is not ideal for large datasets due to limited stack space.
 """
 
 import random
 
 
-def merge(l1, l2):
-    if l1 == []:
-        return l2
-    elif l2 == []:
-        return l1
-    elif l1[0] < l2[0]:
-        return [l1[0]] + merge(l1[1:], l2)
-    return [l2[0]] + merge(l1, l2[1:])
+def merge_rec(left, right):
+    """Recursive Merge"""
+    if left == []:
+        return right
+    elif right == []:
+        return left
+    elif left[0] < right[0]:
+        return [left[0]] + merge(left[1:], right)
+    return [right[0]] + merge(left, right[1:])
+
+
+def merge_iter(left, right):
+    """Iterative Merge"""
+    i = 0  # pointer for left list
+    j = 0  # pointer for right list
+
+    result = []  # sorted result
+
+    # Merge left and right lists
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    # Copy over remaining elements
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
 
 
 def merge_sort(l):
     if len(l) > 1:
         mid = len(l) // 2
-        return merge(merge_sort(l[:mid]), merge_sort(l[mid:]))
+        return merge_iter(merge_sort(l[:mid]), merge_sort(l[mid:]))
     return l
 
 
